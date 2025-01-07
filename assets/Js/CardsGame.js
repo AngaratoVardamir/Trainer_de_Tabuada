1052d1a5eadd2586de58e794b76609d7fcb8180c
Gustavo Augusto <Gus191798@gmail.com>
2025-01-06 21:31:58 -0300

    Adiciona folha de estilo para responsividade e janela inicial do jogo matemático
    + Incluída nova folha de estilo .css para melhorar a responsividade do site.
    
    +Criada janela inicial do game com estrutura básica para cálculos matemáticos.
    
    + Ajustes iniciais no layout visando melhor experiência do usuário.

--- assets/Js/CardsGame.js
+++ assets/Js/CardsGame.js
@@ -1,31 +1,70 @@
 "use strict";
 const Main = document.querySelector('main');
-const Cartas = document.querySelectorAll('.Cards');
 
 // Fazendo as cartas com Js
 
-const ClassCards = [ "Addition", "Subtraction", "Division", "Multiplication" ]; //Classe das Cartas
-const ClassIcones = ["fa-solid fa-plus", "fa-solid fa-minus", "fa-solid fa-divide", "fa-solid fa-xmark"];
+function CreatingLetters() {
+    const ClassCards = [ "Addition", "Subtraction", "Division", "Multiplication" ]; //Classe das Cartas
+    const ClassIcones = ["fa-solid fa-plus", "fa-solid fa-minus", "fa-solid fa-divide", "fa-solid fa-xmark"];
 
-const AmountCards = ClassCards.length; //Quantidade de Cartas =  4
+    const AmountCards = ClassCards.length; //Quantidade de Cartas =  4
 
-for (let index = 0; index < AmountCards; index++) {
-    // Criando a Carta
-    const NewCard = document.createElement("div");
-    NewCard.setAttribute("class", `Cards ${ClassCards[index]}`);
-    // Criando frente da Carta
-    const NewFront = document.createElement("div");
-    NewFront.setAttribute("class", "front");
-    NewCard.appendChild(NewFront);
-    // Criando Verso da Carta
-    const NewBack = document.createElement("div");
-    NewBack.setAttribute("class", "Back");
-    NewCard.appendChild(NewBack);
+    for (let index = 0; index < AmountCards; index++) {
+        // Criando a Carta
+        const NewCard = document.createElement("div");
+        NewCard.setAttribute("class", `Cards ${ClassCards[index]}`);
+        // Criando frente da Carta
+        const NewFront = document.createElement("div");
+        NewFront.setAttribute("class", "front");
+        NewCard.appendChild(NewFront);
+        // Criando Verso da Carta
+        const NewBack = document.createElement("div");
+        NewBack.setAttribute("class", "Back");
+        NewCard.appendChild(NewBack);
 
-    // Criando Icones da Carta
-    const NewIcon = document.createElement("i");
-    NewIcon.setAttribute("class", `${ClassIcones[index]}`);
-    NewFront.appendChild(NewIcon);
-    // Colocando a Carta no Corpo
-    Main.appendChild(NewCard);
+        // Criando Icones da Carta
+        const NewIcon = document.createElement("i");
+        NewIcon.setAttribute("class", `${ClassIcones[index]}`);
+        NewFront.appendChild(NewIcon);
+        // Colocando a Carta no Corpo
+        Main.appendChild(NewCard);
+    }
 }
+CreatingLetters()
+
+const Cartas = [...document.querySelectorAll('.Cards')];
+const Game = document.getElementById('Game');
+
+// Abrindo Janela e fechando as Cartas
+function OpeningWindow_ClosingCards() {
+    Game.style.display = 'flex';
+    Cartas.map((CartaX) =>{
+        CartaX.style.display = 'none';
+    })
+}
+
+
+// Mundaça de Pagina
+Cartas.map((Card) => {
+    Card.addEventListener("click", () =>{
+        console.log(Card.classList[1])
+        switch (Card.classList[1]) {
+            case "Addition":
+                OpeningWindow_ClosingCards()
+                break;
+            case "Subtraction":
+                OpeningWindow_ClosingCards()
+                break;
+            case "Division":
+                OpeningWindow_ClosingCards()
+                break;
+            case "Multiplication":
+                OpeningWindow_ClosingCards()
+                break;
+
+            default:
+                console.log("Pobrema na Carta");
+                break;
+        }
+    })
+})
